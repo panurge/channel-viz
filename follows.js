b06f3e909202753580c99a609f8aaaf7db16cc0c
panurge <keith.jones@itv.com>
2014-10-29 18:31:49 +0000

    Update follows.js

--- follows.js
+++ follows.js
@@ -14,7 +14,7 @@
 
 	var defaultKey		= 'bNMXcunAs93aNEms1HTmLhatDZrAoKXWb8WvYlsheO28Qntf', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1254033461], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'Plooper', // Replaces Xively logo in the header
+		applicationName	= 'Central Heating Controller', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
